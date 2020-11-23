bd98da23bb74a3f40b6f7b2b40f30ce79260357a
Felipe <felipec.barroso97@gmail.com>
2020-11-23 19:49:20 -0300

    Primeira classe

---
+++ Xadrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
+            Console.ReadLine();
+        }
+    }
+}
