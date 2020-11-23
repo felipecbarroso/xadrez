9e33511bee957d29246758fd741bd1106eae8c84
Felipe <felipec.barroso97@gmail.com>
2020-11-23 20:19:00 -0300

    Classe Tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace Xadrez {
     class Program {
         static void Main(string[] args) {
-            Posicao P;
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(P);
             Console.ReadLine();
         }
     }
---
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Tabuleiro {
+    class Tabuleiro {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas { get; set; }
+
+        public Tabuleiro(int linhas, int colunas) {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pecas = new Peca[linhas, colunas];
+        }
+    }
+}
