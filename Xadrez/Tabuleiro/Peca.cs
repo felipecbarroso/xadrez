c012fac60f392f4cbafef9893e3724ffe52e0bc8
Felipe <felipec.barroso97@gmail.com>
2020-11-24 02:14:30 -0300

    Criando a classe PosicaoXadrez

--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -1,17 +1,19 @@
 ﻿
 
-namespace Tabuleiro {
+namespace tabuleiro {
     class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qtoMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab) {
-            this.posicao = posicao;
+        public Peca(Tabuleiro tab, Cor cor) {
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qtoMovimentos = 0;
         }
+
+       
     }
 }
